030a8e57b8906cdcba28fdd24477eda265d1ed62
iusd333-lala <iusd333@gmail.com>
2025-11-30 19:33:53 +0800

    Update StoneMasonKarel.py

--- stanCode_Projects/01/StoneMasonKarel.py
+++ stanCode_Projects/01/StoneMasonKarel.py
@@ -1,6 +1,6 @@
 """
 File: StoneMasonKarel.py
-Name: 
+Name: Cindy
 --------------------------------
 At present, the StoneMasonKarel file does nothing.
 Your job in the assignment is to add the necessary code to
